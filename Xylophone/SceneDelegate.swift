e01c9b3b7d77dcab397be717989086c0488f1520
Saul Rivera <saulriveraal@icloud.com>
2020-04-04 15:04:19 -0600

    Xylophone Completed

--- Xylophone/SceneDelegate.swift
+++ Xylophone/SceneDelegate.swift
@@ -2,7 +2,7 @@
 //  SceneDelegate.swift
 //  Xylophone
 //
-//  Created by Angela Yu on 28/06/2019.
+//  Created by Saul Rivera on 28/06/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -2,20 +2,48 @@
 //  ViewController.swift
 //  Xylophone
 //
-//  Created by Angela Yu on 28/06/2019.
+//  Created by Saul Rivera on 28/06/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let title = sender.titleLabel?.text ?? "N/A"
+        print(title)
+        playSound(tone: title)
+        
+        sender.alpha = sender.alpha / 3
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            sender.alpha = sender.alpha * 3
+        }
+    }
     
-
+    private func playSound(tone: String) {
+        guard let url = Bundle.main.url(forResource: tone, withExtension: "wav") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url)
+            
+            guard let player = player else { return }
+            player.play()
+        } catch let error {
+            print(error)
+        }
+    }
 }
 
